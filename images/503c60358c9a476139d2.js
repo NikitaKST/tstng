166147951413df675c61037cdf2701fbb17f0558
AppVeyor <ci@appveyor.com>
2024-02-08 15:32:45 +0000

    5d77d7b

---
+++ images/503c60358c9a476139d2.js
@@ -0,0 +1,1 @@
+import"./css/style.css";import CreditCardForm from"./components/CreditCardForm";const container=document.querySelector(".container"),form=new CreditCardForm(container);form.bindToDom();
